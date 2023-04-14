52bf74cddd8420d6d36dcfdecdee07aeb9d1232a
Ross <rabik95@bk.ru>
2023-04-14 13:41:29 +0300

    Добавлен футер

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,6 +1,15 @@
 export const en = {
-    "locale-name_en": "Eng",
-    "locale-name_ru": "Ru",
-    welcome: "welcome",
-    header: [{text : "My IVI", path: "/"}, {text : "What's new", path: "/"}, {text : "Movies", path: "/"}, {text : "Series", path: "/"}, {text : "Cartoons", path: "/"}, {text : "TV+", path: "/"}]
+  "locale-name_en": "Eng",
+  "locale-name_ru": "Ru",
+  welcome: "welcome",
+  header: [
+    { text: "My IVI", path: "/", img: "https://static.vecteezy.com/system/resources/previews/013/743/156/non_2x/white-house-icon-png.png", },
+    { text: "Catalog", path: "/",
+    img: "https://img.icons8.com/ios-glyphs/30/FFFFFF/folder-invoices--v1.png", },
+    { text: "Search", path: "/",
+    img: "https://img.icons8.com/ios-filled/50/FFFFFF/search--v1.png", },
+    { text: "TV+", path: "/",
+    img: "https://img.icons8.com/ios-glyphs/30/FFFFFF/tv.png", },
+    { text: "More", path: "/", img: "https://img.icons8.com/ios-filled/50/FFFFFF/more.png", },
+  ],
 };
