7a5998adab01400ac71d39d97cd17c9ec089f92d
VladimirSukhoparov <sukhoparov.v.n@gmail.com>
2023-04-15 00:29:27 +0500

    запустил storybook, обнулил стили

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from "../styles/components/footer/Footer.module.scss";
 import { useLocale } from '../hooks/useLocale';
 import Link from 'next/link';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Footer = () => {
     const translation = useLocale();
@@ -12,8 +12,8 @@ const Footer = () => {
                 <ul>
                     {translation.header.map((el) => {
                         return (
-                            <Link href={el.path}>
-                                <li key={el.text}>
+                            <Link href={el.path} key={el.text}>
+                                <li >
                                     <img src={el.img} alt="" />
                                     <span>{el.text}</span>
                                 </li>
